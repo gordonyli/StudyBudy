861f4d40c137f569bcc7acf45c55298f5929619e
gordonyli <gordonyli@gmail.com>
2016-09-24 12:22:13 -0400

    login working

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"twitter_api_key" : "kYHSEtFujG3ybYHFyMoKziNeY",
+	"twitter_api_secret" : "7BtdOfoY5TylNiK53vL7R0DNHDNcXrYHX9sLDc5L4EiQ3sIaP5",
+	"callback_url" : "http://localhost:3000/auth/twitter/callback",
+	"use_database" : "true",
+	"host":	"localhost",
+	"username" : "root",
+	"password" : "mysqlpass",
+	"database" : "studybuddy"
+}
